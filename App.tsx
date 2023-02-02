083bc0debba7f1f37a73fe11ca54961859dbd89e
Bisnu kundu <bisnukundu@gmail.com>
2023-02-02 17:46:46 +0600

    website to android using webview

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React, {useEffect, useRef} from 'react';
+import {Text, View, Platform, BackHandler} from 'react-native';
+import {WebView} from 'react-native-webview';
+function App() {
+  const webViewRef = useRef(null);
+  const onBackPress = ()=>{
+    if (webViewRef.current) {
+      webViewRef.current.goBack();
+    }
+    return false;
+  }
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      BackHandler.addEventListener('hardwareBackPress', () => );
+
+    }
+  }, []);
+  return (
+    <WebView
+      ref={webViewRef}
+      scalesPageToFit={true}
+      source={{uri: 'https://sch.itcampussoft.com'}}
+    />
+  );
+}
+
+export default App;
